65f9da3f75c08a84590597fce57bff71eef08164
MIZUNE Pine <pinemz@gmail.com>
2014-05-19 15:20:34 +0900

    Support logout

--- KUTPortalOnChrome/background.js
+++ KUTPortalOnChrome/background.js
@@ -1,11 +1,13 @@
 
 // Firefox 27 / Windows 7 x64
-var USER_AGENT = "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0";
+var USER_AGENT = "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0";
+
+var PORTAL_ENTER_URL = 'http://portal.kochi-tech.ac.jp/';
 
 // UserAgent を偽装する
 chrome.webRequest.onBeforeSendHeaders.addListener(
   function(details){
-    for(var i=0; i<details.requestHeaders.length; i++){
+    for(var i = 0; i < details.requestHeaders.length; i++){
       var header = details.requestHeaders[i];
       
       if(header.name == "User-Agent"){
@@ -14,7 +16,7 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
       }
     }
     
-    return { requestHeaders:details.requestHeaders };
+    return { requestHeaders: details.requestHeaders };
   },
   {
     urls: [
@@ -27,4 +29,57 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
     "requestHeaders",
     "blocking"
   ]
+);
+
+chrome.webRequest.onHeadersReceived.addListener(
+  function(details){
+    // Cookie を削除
+    
+    // Cookie 削除対象 URL
+		var cookie_urls = [
+			"https://portal1.kochi-tech.ac.jp/",
+			"https://portal2.kochi-tech.ac.jp/",
+			"https://idp.kochi-tech.ac.jp/idp"
+		];
+		
+		cookie_urls.forEach(function(url){
+			var cookie_details = {
+				url: url
+			};
+			
+			// 全ての Cookie の情報を取得
+			chrome.cookies.getAll(
+				cookie_details,
+				function(cookies){
+					// 全ての Cookie を削除
+					cookies.forEach(function(cookie){
+						chrome.cookies.remove({ url: url, name: cookie.name });
+					});
+				}
+			);
+		});
+    
+    // リダイレクト先を変更
+    for(var i = 0; i < details.responseHeaders.length; i++){
+      var header = details.responseHeaders[i];
+      
+      if(header.name == "Location"){
+        header.value = PORTAL_ENTER_URL;
+        break;
+      }
+    }
+    
+    return { responseHeaders: details.responseHeaders };
+  },
+	{
+		urls: [
+			// ログアウトページ
+			"https://portal1.kochi-tech.ac.jp/Portal/logout.aspx",
+			"https://portal2.kochi-tech.ac.jp/Portal/logout.aspx"
+		]
+	},
+	[
+    "responseHeaders",
+    "blocking"
+	]
 );

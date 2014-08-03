e84c95d6bb28f61f700a3b948c4c12d717e68dee
MIZUNE Pine <pinemz@gmail.com>
2014-08-03 13:23:12 +0900

    Convert tab to space

--- KUTPortalOnChrome/background.js
+++ KUTPortalOnChrome/background.js
@@ -36,28 +36,28 @@ chrome.webRequest.onHeadersReceived.addListener(
     // Cookie を削除
     
     // Cookie 削除対象 URL
-		var cookie_urls = [
-			"https://portal1.kochi-tech.ac.jp/",
-			"https://portal2.kochi-tech.ac.jp/",
-			"https://idp.kochi-tech.ac.jp/idp"
-		];
-		
-		cookie_urls.forEach(function(url){
-			var cookie_details = {
-				url: url
-			};
-			
-			// 全ての Cookie の情報を取得
-			chrome.cookies.getAll(
-				cookie_details,
-				function(cookies){
-					// 全ての Cookie を削除
-					cookies.forEach(function(cookie){
-						chrome.cookies.remove({ url: url, name: cookie.name });
-					});
-				}
-			);
-		});
+    var cookie_urls = [
+      "https://portal1.kochi-tech.ac.jp/",
+      "https://portal2.kochi-tech.ac.jp/",
+      "https://idp.kochi-tech.ac.jp/idp"
+    ];
+    
+    cookie_urls.forEach(function(url){
+      var cookie_details = {
+        url: url
+      };
+      
+      // 全ての Cookie の情報を取得
+      chrome.cookies.getAll(
+        cookie_details,
+        function(cookies){
+          // 全ての Cookie を削除
+          cookies.forEach(function(cookie){
+            chrome.cookies.remove({ url: url, name: cookie.name });
+          });
+        }
+      );
+    });
     
     // リダイレクト先を変更
     for(var i = 0; i < details.responseHeaders.length; i++){
@@ -71,15 +71,15 @@ chrome.webRequest.onHeadersReceived.addListener(
     
     return { responseHeaders: details.responseHeaders };
   },
-	{
-		urls: [
-			// ログアウトページ
-			"https://portal1.kochi-tech.ac.jp/Portal/logout.aspx",
-			"https://portal2.kochi-tech.ac.jp/Portal/logout.aspx"
-		]
-	},
-	[
+  {
+    urls: [
+      // ログアウトページ
+      "https://portal1.kochi-tech.ac.jp/Portal/logout.aspx",
+      "https://portal2.kochi-tech.ac.jp/Portal/logout.aspx"
+    ]
+  },
+  [
     "responseHeaders",
     "blocking"
-	]
+  ]
 );
